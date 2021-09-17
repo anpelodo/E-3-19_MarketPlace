31f1c229597344b01932a5588e2b84811c5263f5
Alejandro Largo <largoalejo@gmail.com>
2021-09-17 09:39:32 -0500

    Configuracion basica del proyecto con vue@cli, recomendaciones en el readme

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Temporal />
+</template>
+
+<script>
+import { defineAsyncComponent } from "@vue/runtime-core";
+
+export default {
+  name: "App",
+  components: {
+    Temporal: defineAsyncComponent(() => import("@/components/Temporal.vue")),
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Temporal.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Marketplace</h1>
+  <p>Esta vista es temporal, mientras se define la estructura del proyecto.</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
