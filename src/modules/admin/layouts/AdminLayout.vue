3b9a42ac390be20ec189a0871222d0b384f54cc1
Juan Esteban Londoño <jestebanlondono11@gmail.com>
2021-10-03 22:49:26 -0500

    se agrego el router a la vista del admin

--- src/modules/admin/layouts/AdminLayout.vue
+++ src/modules/admin/layouts/AdminLayout.vue
@@ -1,29 +1,36 @@
 <template>
   <!-- <router-view></router-view> -->
+  <nav-bar/>
+  <br>
   <Admin/>
   <add-product/>
   <delete-product/>
   <edit-product/>
+
 </template>
 
 <script>
 // import Counter from './components/Counter.vue'
-import Admin from './modules/admin/components/Admin.vue'
-import AddProduct from './modules/admin/components/AddProduct.vue'
-import DeleteProduct from './modules/admin/components/DeleteProduct.vue'
-import EditProduct from './modules/admin/components/EditProduct.vue'
+import Admin from '../components/Admin.vue'
+import AddProduct from '../components/AddProduct.vue'
+import DeleteProduct from '../components/DeleteProduct.vue'
+import EditProduct from '../components/EditProduct.vue'
+import NavBar from '../components/NavBar.vue'
+
 
 
 
 export default {
   
-  name: 'App',
+ 
   components: {
     //Counter,
     Admin,
     AddProduct,
     DeleteProduct,
-    EditProduct
+    EditProduct,
+    NavBar
+    
    
 
   }
---
+++ src/modules/admin/router/index.js
@@ -0,0 +1,7 @@
+export default {
+  name: "admin",
+  component: () => import(/* webpackChunkName: "admin" */ "../layouts/AdminLayout.vue"),
+  children: [
+    
+  ]
+};
