12b65060b5fda813c48f9252c0b53cfe67eebe64
anpelodo <53635026+anpelodo@users.noreply.github.com>
2021-10-15 02:04:18 -0500

    Se agrega el backend al repositorio.

---
+++ Backend/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+//const { url } = require('inspector');
+
+const app = express();
+
+//CONEXION A DB
+const mongoose = require("mongoose");
+const uri = "mongodb://127.0.0.1:27017/marketplace";
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose.connect(uri, options).then(
+  () => {
+    console.log("Conectado a DB");
+  },
+  err => {
+    console.log("Error conectando a la base de datos: ", err);
+  }
+);
+
+//MIDDLEWARE's
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Main Router
+app.use("/api", require("./routes/producto"));
+
+const history = require("connect-history-api-fallback");
+app.use(history());
+app.use(express.static(path.join(__dirname, "public")));
+
+//PUERTO
+app.set("puerto", process.env.PORT || 3002);
+app.listen(app.get("puerto"), function() {
+  console.log("Example app listening on port " + app.get("puerto"));
+});
---
+++ Backend/routes/producto.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+
+const Producto = require("../models/producto");
+
+//Agregar un producto
+
+router.post("/nuevo-producto", async (req, res) => {
+  const body = req.body;
+
+  try {
+    const productoDB = await Producto.create(body);
+    res.status(201).json(productoDB);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ocurrio un error",
+      err
+    });
+  }
+});
+
+//obtener documento por id
+router.get("/producto/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const productoDB = await Producto.findOne({ _id });
+
+    return productoDB ? res.json(productoDB) : res.status(204).json({});
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ocurrio un error",
+      err
+    });
+  }
+});
+
+//obtener todo los documentos
+router.get("/producto", async (req, res) => {
+  try {
+    const productoDB = await Producto.find();
+    res.json(productoDB);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ocurrio un error",
+      err
+    });
+  }
+});
+
+//delete
+
+router.delete("/producto/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const productoDB = await Producto.findByIdAndDelete({ _id });
+
+    if (!productoDB) {
+      return res.status(400).json({
+        mensaje: "No se encontro el id indicado",
+        error
+      });
+    } else {
+      return res.status(200).json({
+        id: _id,
+        status: true
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ocurrio un error",
+      err
+    });
+  }
+});
+
+//update
+
+router.put("/producto/:id", async (req, res) => {
+  const _id = req.params.id;
+  const body = req.body;
+
+  try {
+    const productoDB = await Producto.findByIdAndUpdate(_id, body, {
+      new: true
+    });
+    res.json(productoDB);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ocurrio un error",
+      err
+    });
+  }
+});
+
+//Exportamos la configuracion de express app
+module.exports = router;
